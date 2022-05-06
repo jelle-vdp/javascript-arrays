893bf92dac3026c5aefaa30828c98d3b906988df
jelle-vdp <jellevandepoel@gmail.com>
2022-05-06 15:14:55 +0200

    finished exercise eleven

--- 11-rand-array-stats-Optional/script.js
+++ 11-rand-array-stats-Optional/script.js
@@ -11,6 +11,33 @@
 
 (function() {
 
-    // your code here
+    const numArr = [
+        25,
+        36,
+        45,
+        12,
+        36,
+        98,
+        24,
+        25,
+        69,
+        2
+    ];
+
+    document.getElementById("run").addEventListener("click", () => {
+        
+        numArr.forEach((num, index) => {
+            let currId = "n-" + (index + 1).toString();
+            let value = num.toString();
+            document.getElementById(currId).innerText = value;
+        });
+
+        document.getElementById("min").innerText = Math.min(...numArr);
+        document.getElementById("max").innerText = Math.max(...numArr);
+        const sum = numArr.reduce((prev, next) => prev + next, 0);
+        document.getElementById("sum").innerText = sum;
+        document.getElementById("average").innerText = sum / numArr.length;
+
+    });
 
 })();
